96469552634a39a1819c3dd80d0210151da59cf9
Ephemeral25 <clemdem61@gmail.com>
2019-12-23 22:01:28 +0100

    Added new feature for list of envies

--- app/src/main/java/com/example/fragmentedapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/fragmentedapp/ui/home/HomeFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.fragmentedapp.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -22,9 +23,12 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+
         homeViewModel.text.observe(this, Observer {
-            textView.text = it
+            buttonMenuAddFood.setOnClickListener {
+                foodList.text = "${foodList.text} \n ${foodEdit.text}"
+                foodEdit.text.clear()
+            }
         })
         return root
     }
